e00fab1a3a543b313fc99ac4c82e58581180a97d
Philip-ZENG <892542757@qq.com>
2023-09-01 16:18:14 +0800

    Configured docker compose and checks to wait for db

--- Dockerfile
+++ Dockerfile
@@ -22,11 +22,15 @@ ARG DEV=false
 # 5. add new user (other than root user) inside the docker image; for safety reasons
 RUN python -m venv /py && \ 
   /py/bin/pip install --upgrade pip && \
+  apk add --update --no-cache postgresql-client && \
+  apk add --update --no-cache --virtual .tmp-build-deps \
+    build-base postgresql-dev musl-dev && \
   /py/bin/pip install -r /tmp/requirements.txt && \
   if [ $DEV = "true" ]; \
     then /py/bin/pip install -r /tmp/requirements.dev.txt ; \
   fi && \
   rm -rf /tmp && \
+  apk del .tmp-build-deps && \
   adduser \ 
     --disabled-password \
     --no-create-home \
---
+++ app/core/tests/test_commands.py
@@ -0,0 +1,37 @@
+"""
+Test custom Django management command.
+"""
+from unittest.mock import patch
+
+from psycopg2 import OperationalError as Psycopg2Error
+
+from django.core.management import call_command
+from django.db.utils import OperationalError
+from django.test import SimpleTestCase
+
+
+# mock the management command method to simulate the response of the database
+@patch('core.management.commands.wait_for_db.Command.check')
+class CommandTest(SimpleTestCase):
+    """Test commands."""
+
+    def test_wait_for_db_ready(self, patched_check):
+        """Test waiting for database if database ready."""
+        patched_check.return_value = True
+
+        call_command('wait_for_db')
+
+        patched_check.assert_called_once_with(databases=['default'])
+
+    @patch('time.sleep')
+    def test_wait_for_db_delay(self, patched_sleep, patched_check):
+        """Test waiting for database when getting OperationalError."""
+        # raise 2 Psycopg2Error, then 3 OperationalError, then True
+        # to simulate the database preparation process
+        patched_check.side_effect = [Psycopg2Error] * 2 + \
+            [OperationalError] * 3 + [True]
+
+        call_command('wait_for_db')
+
+        self.assertEqual(patched_check.call_count, 6)
+        patched_check.assert_called_with(databases=['default'])
